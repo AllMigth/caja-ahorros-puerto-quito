9d538d8d19741ed1c62e7c15f77b0bc304e94d24
AllMigth <jgarzon292@gmail.com>
2026-05-28 21:49:34 -0500

    Fix: Use single DATABASE_URL with sslmode

--- src/main/java/com/caja/ahorros/AhorrosApplication.java
+++ src/main/java/com/caja/ahorros/AhorrosApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AhorrosApplication {
 
 	public static void main(String[] args) {
+		System.out.println("DB_URL: " + System.getenv("DATABASE_URL"));
 		SpringApplication.run(AhorrosApplication.class, args);
 	}
 
